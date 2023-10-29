aecb22f1f4b0dd65bb83f1fe644058f444d8e4e3
Дмитрий Жуков <dmitrijzukov@MacBook-Air-Dmitrij.local>
2023-10-29 18:15:56 +0400

    feat(build): vite init config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+import { resolve } from 'path'
+
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-  ],
+  plugins: [tsconfigPaths()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'output/index.js'),
+      // TODO: from envs
+      name: 'material-ui-my-icons',
+      formats: ['umd'],
+    },
+    outDir: 'lib',
+  },
 })
